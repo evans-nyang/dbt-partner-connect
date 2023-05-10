de812b210670ac34855a382f3e4982b6e428bab1
Johnevans <certevans102@gmail.com>
2023-05-10 00:18:32 +0000

    Modularization of the models

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -2,4 +2,5 @@ select
     id as customer_id,
     first_name,
     last_name
+
 from  crude.jaffle_shop.customers
--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -3,4 +3,5 @@ select
     user_id as customer_id,
     order_date,
     status
+
 from crude.jaffle_shop.orders
